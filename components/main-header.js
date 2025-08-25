fda7d1bbb7076b02529a8cf85b492a0ceaf011f9
umesh <utkrisht@gmail.com>
2025-08-25 19:03:29 +0530

    change the deafult img to nextJS Image Component

--- components/main-header.js
+++ components/main-header.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
+
+
+
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css'
+
+
+
+// this Image is providing a optimized version of image to use in brower with features like lazy loading and priority property setup 
+
 
 export default function MainHeader(){
     return <header className={classes.header}>
         <Link className={classes.logo} href='/'>
-           <img src={logoImg.src} alt=" a plate with food on it"  />
+           {/* <img src={logoImg.src} alt=" a plate with food on it"  /> */}
+           <Image src={logoImg} alt='a plate with food on it.' priority />
            NextLevel Food
         </Link>
         <nav className={classes.nav}>
