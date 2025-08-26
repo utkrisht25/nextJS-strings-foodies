41fb1aa0fd1489fe9fcdd015c641085a41ec14fc
umesh <utkrisht@gmail.com>
2025-08-26 16:03:53 +0530

    update main header and root page.js file

---
+++ components/main-header/main-header.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import Image from "next/image";
+
+
+
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css'
+import MainHeaderBackground from "./main-header-background";
+
+
+
+// this Image is providing a optimized version of image to use in brower with features like lazy loading and priority property setup 
+
+
+export default function MainHeader(){
+    return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
