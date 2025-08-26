37f86081e14faef7fec13dad013a6a74024082c4
umesh <utkrisht@gmail.com>
2025-08-26 20:50:02 +0530

    create database with sqlite and fetch the meals and render them on the meals page

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -6,7 +6,7 @@ export default function MealsGrid({meals}){
         <ul className={classes.meals}>
             {meals.map((meal)=>(
                 <li key={meal.id}>
-                    <MealItem {...meals} />
+                    <MealItem {...meal} />
                 </li>
             ))}
         </ul>
